af57a16c98ea580ff4552aac09767d62149f704a
y4553r <abougueniyasser@outlook.com>
2019-11-08 12:04:18 +0100

    Updates

---
+++ precache-manifest.24216a693058b29dca9eb7cd450ea723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51ccc90499a9af031f86e4508fd3919",
+    "url": "/react-colors-project/index.html"
+  },
+  {
+    "revision": "7283ba9087b1a0e40d04",
+    "url": "/react-colors-project/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "566ea27786533d60b327",
+    "url": "/react-colors-project/static/css/main.b4067545.chunk.css"
+  },
+  {
+    "revision": "7283ba9087b1a0e40d04",
+    "url": "/react-colors-project/static/js/2.e22e0a95.chunk.js"
+  },
+  {
+    "revision": "566ea27786533d60b327",
+    "url": "/react-colors-project/static/js/main.1daccf69.chunk.js"
+  },
+  {
+    "revision": "111bb46aa48cbab96bab",
+    "url": "/react-colors-project/static/js/runtime-main.059feabb.js"
+  },
+  {
+    "revision": "e9f6350478adf4908e58f027330bfbc0",
+    "url": "/react-colors-project/static/media/bg.e9f63504.svg"
+  }
+]);
